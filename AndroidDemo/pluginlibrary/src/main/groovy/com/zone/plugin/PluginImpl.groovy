298f7d3080dc4a321c9db34dacae4a08a077995b
xiuwen.zhan <xiuwen.zhan@tcl.com>
2019-10-25 10:20:58 +0800

    插件

---
+++ AndroidDemo/pluginlibrary/src/main/groovy/com/zone/plugin/PluginImpl.groovy
@@ -0,0 +1,11 @@
+package com.zone.plugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class PluginImpl implements Plugin<Project> {
+    void apply(Project project) {
+        project.task('testTask') << {
+            println "Hello gradle plugin"
+        }
+    }
+}
